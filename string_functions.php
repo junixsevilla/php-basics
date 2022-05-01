f41d79e16a47e6f326d50aac082046d8f2ea3701
Junix Aranco <jaranco@gmail.com>
2022-05-01 23:49:35 +1200

    add string function

---
+++ string_functions.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>String Functions</title>
+</head>
+<body>
+
+<?php
+
+echo ("\"The Quick Brown Fox!\" <br> ");
+echo "<br>";
+echo "Count: ";
+echo strlen("The Quick Brown Fox!");
+
+echo "<br>";
+echo "Uppercase: ";
+echo strtoupper("The Quick Brown Fox!");
+echo "<br>";
+echo "Lowercase: ";
+echo strtolower("The Quick Brown Fox!");
+
+?>
+    
+</body>
+</html>
